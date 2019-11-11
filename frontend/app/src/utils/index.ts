b410e51ac32badefd9f264e5fc2da4f4b1d71daa
badm3t <b4dm3t@gmail.com>
2019-11-11 17:39:18 +0700

    add tests for utils

--- frontend/app/src/utils/index.ts
+++ frontend/app/src/utils/index.ts
@@ -1,6 +1,6 @@
-export const average = (min, max) => Math.floor((max - min) / 2);
+export const average = (min, max) => Math.floor((max - min) / 2) + min;
 
-export const positive = obj => {
+export const filterKeys = obj => {
   const res = [];
   for (let key in obj) {
     if (obj[key]) res.push(key);
--- frontend/app/src/utils/utils.test.ts
+++ frontend/app/src/utils/utils.test.ts
@@ -1,8 +1,20 @@
-import { average } from './index';
+import { average, filterKeys } from './index';
 
 describe("average function", () => {
   test('find average value between min, max', () => {
-    expect(average(1, 3)).toBe(1);
+    expect(average(1, 3)).toBe(2);
+    expect(average(10, 100)).toBe(55);
   });
 });
 
+describe("filterKeys function", () => {
+  test('get keys with "true" value', () => {
+    const obj = {
+      'KEY1': true,
+      'KEY2': true,
+      'KEY3': false,
+    }
+    const res = ['KEY1', 'KEY2'];
+    expect(filterKeys(obj)).toEqual(res);
+  });
+});
